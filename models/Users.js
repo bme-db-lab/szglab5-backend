3c81fc3e294f8f9a2c53b9c538abe06898421e0b
Mark Fejes <fejes.mark92@gmail.com>
2017-05-05 15:58:04 +0200

    add admin user

--- models/Users.js
+++ models/Users.js
@@ -7,7 +7,18 @@ module.exports = (sequelize, DataTypes) => {
     sshPublicKey: DataTypes.STRING,
     // Student specific attributes
     neptun: DataTypes.STRING,
-    university: DataTypes.STRING
+    university: DataTypes.STRING,
+    // Staff specific attributes
+    email_official: DataTypes.STRING,
+    mobile: DataTypes.STRING,
+    title: DataTypes.STRING,
+    printSupport: DataTypes.STRING,
+    // temporary attributes
+    studentgroup_id: DataTypes.STRING,
+    classroom: DataTypes.STRING,
+    spec: DataTypes.STRING,
+    exercises: DataTypes.STRING,
+    ownedExerciseID: DataTypes.STRING
   }, {
     classMethods: {
       associate: (models) => {
