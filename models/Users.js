d7620b9e4890afed7a16a3f17c536ad29e949c47
Mark Fejes <fejes.mark92@gmail.com>
2017-05-06 20:53:46 +0200

    add mocked role handling

--- models/Users.js
+++ models/Users.js
@@ -5,6 +5,8 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     email: DataTypes.STRING,
     sshPublicKey: DataTypes.STRING,
+    // TODO temporary role management
+    role: DataTypes.STRING,
     // Student specific attributes
     neptun: DataTypes.STRING,
     university: DataTypes.STRING,
