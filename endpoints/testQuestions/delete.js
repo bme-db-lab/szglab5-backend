91f51a340a40fe10421fcc507b0b6995f265981e
Sgánetz Bence <voidxb@gmail.com>
2017-05-07 22:26:54 +0200

    Delete TestQuestion

---
+++ endpoints/testQuestions/delete.js
@@ -0,0 +1,38 @@
+const { genErrorObj } = require('../../utils/utils.js');
+const { genJSONApiResByRecord, checkIfExist } = require('../../utils/jsonapi.js');
+const { getDB } = require('../../db/db.js');
+
+module.exports = (req, res) => {
+  try {
+    const reqId = req.params.id;
+    const reqIdNum = parseInt(reqId, 10);
+    if (isNaN(reqId)) {
+      res.status(400).send(genErrorObj('Requested id is not a number'));
+      return;
+    }
+
+    const db = getDB();
+    db.TestQuestions.destroy({ where: { id: reqIdNum }})
+      .then(() => {
+        res.status(200).send();
+      })
+      .catch((err) => {
+        if (err.notFound) {
+          res.status(404).send(genErrorObj(err.message));
+          return;
+        }
+        res.status(500).send(genErrorObj(err.message));
+      });
+  } catch (err) {
+    res.status(500).send(genErrorObj(err.message));
+  }
+};
+
+/**
+ * @api {get} /deliverables/:id Delete TestQuestion
+ * @apiName Delete
+ * @apiGroup TestQuestions
+ * @apiDescription Delete text question entry by id
+ *
+ * @apiParam {Number} [id] Test question's id
+ */
--- endpoints/testQuestions/index.js
+++ endpoints/testQuestions/index.js
@@ -1,9 +1,11 @@
 const addTestQuestion = require('./add.js');
+const deleteTestQuestions = require('./delete.js');
 const getTestQuestions = require('./get.js');
 const listTestQuestions = require('./list.js');
 const updateTestQuestions = require('./update.js');
 
 module.exports = (app) => {
+  app.delete('/testQuestions/:id', deleteTestQuestions);
   app.get('/testQuestions', listTestQuestions);
   app.get('/testQuestions/:id', getTestQuestions);
   app.patch('/testQuestions/:id', updateTestQuestions);
