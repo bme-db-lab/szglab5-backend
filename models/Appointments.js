8747ef9b6b0038ede6605db0f5910c6daca4fd53
mordekk <mordekk@gmail.com>
2017-05-14 20:05:19 +0200

    Fixed merging bug.
    Signed-off-by: mordekk <mordekk@gmail.com>

--- models/Appointments.js
+++ models/Appointments.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
--  const Appointments = sequelize.define('Appointments', {
--    date: DataTypes.DATE,
--    location: DataTypes.TEXT,
--    type: DataTypes.STRING,
--  }, {
--    classMethods: {
--      associate: (models) => {
--        Appointments.belongsTo(models.EventTemplates);
--        Appointments.belongsTo(models.StudentGroups);
--      }
--    }
--  });
--
--  return Appointments;
--};
+  const Appointments = sequelize.define('Appointments', {
+    date: DataTypes.DATE,
+    location: DataTypes.TEXT,
+    type: DataTypes.STRING,
+  }, {
+    classMethods: {
+      associate: (models) => {
+        Appointments.belongsTo(models.EventTemplates);
+        Appointments.belongsTo(models.StudentGroups);
+      }
+    }
+  });
+
+  return Appointments;
+};
