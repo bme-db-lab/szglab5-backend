e6a26d1409ce1ea9bdafe2bc1e7bedfcec6cddf0
Mark Fejes <fejes.mark92@gmail.com>
2017-05-18 21:00:12 +0200

    add some to demo.seed.json

--- models/DeliverableTemplates.js
+++ models/DeliverableTemplates.js
@@ -1,6 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const DeliverableTemplates = sequelize.define('DeliverableTemplates', {
     description: DataTypes.STRING,
+    type: DataTypes.STRING
   }, {
     classMethods: {
       associate: (models) => {
