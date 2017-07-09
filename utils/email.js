67746a4ea6628910683da6e34d1c8e7ac5493a1b
Mark Fejes <fejes.mark92@gmail.com>
2017-07-09 11:54:56 +0200

    add utils/email stub functions

---
+++ utils/email.js
@@ -0,0 +1,17 @@
+function sendMail(to, content) {
+  // TODO
+}
+
+function subscribeToMailList(address, list) {
+  // TODO
+}
+
+function unSubscribeToMailList(address, list) {
+  // TODO
+}
+
+module.exports = {
+  sendMail,
+  subscribeToMailList,
+  unSubscribeToMailList
+};
