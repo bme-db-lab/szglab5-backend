997e3c1fd6d91c2815f67eb7d8e6952e075e6a35
Szasz Marton <szaszm01@gmail.com>
2017-07-12 23:36:35 +0200

    Initial email sending support
    The code really needs some love that lasts more than half an hour.
    Need to get hardcoded values off email.js to config.
    Need to make sendMail async, because email sending is slow.

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,34 @@
+"use strict";
+const nodemailer = require('nodemailer');
+
+
 function sendMail(to, content) {
-  // TODO
+	// TODO: get hardcoded values out to the config, make this async
+	let transporter = nodemailer.createTransport({
+		host: 'szaszm.tk',
+		port: 25,
+		secure: false,
+		auth: {
+			user: 'szglab5',
+			pass: 'szglab5pass'
+		}
+	});
+
+	let mailOptions = {
+		from: 'Adatbazisok labor adminisztracios rendszer <marci@szaszm.tk>',
+		to: to,
+		subject: 'Adatbazisok labor rendszeruzenet',
+		text: content,
+		html: content
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if(error) {
+			console.log(error);
+		}
+		console.log('Message %s sent: %s', info.messageId, info.response);
+	});
+
 }
 
 function subscribeToMailList(address, list) {
