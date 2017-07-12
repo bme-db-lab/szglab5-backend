f1514e2f129f291204e4e3d71a05af9d1cd6322f
Sgánetz Bence <voidxb@gmail.com>
2017-07-13 01:58:35 +0200

    Deliverable's student endpoint

--- endpoints/deliverables/index.js
+++ endpoints/deliverables/index.js
@@ -2,6 +2,7 @@ const auth = require('../../middlewares/auth.js');
 const getDeliverables = require('./get.js');
 const getDeliverableCorrector = require('./getCorrector.js');
 const getDeliverableTemplate = require('./getTemplate.js');
+const getDeliverableStudent = require('./getStudent.js');
 const listDeliverables = require('./list.js');
 const updateDeliverables = require('./update.js');
 
@@ -10,6 +11,7 @@ module.exports = (app) => {
   app.get('/deliverables/:id', getDeliverables);
   app.get('/deliverables/:id/corrector', getDeliverableCorrector);
   app.get('/deliverables/:id/template', getDeliverableTemplate);
+  app.get('/deliverables/:id/student', getDeliverableStudent);
   app.get('/deliverables', listDeliverables);
   app.patch('/deliverables/:id', updateDeliverables);
 };
