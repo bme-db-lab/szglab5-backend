be7d74e5ed961fadef236fb35af70afa7a34e41e
Szasz Marton <szaszm01@gmail.com>
2017-07-15 19:47:48 +0200

    converted utils.sendMail to return a promise

--- utils/email.js
+++ utils/email.js
@@ -4,31 +4,38 @@ const config = require('../config/config.js');
 
 
 function sendMail(to, content) {
-	let transporter = nodemailer.createTransport({
-		host: config.mailer.host,
-		port: config.mailer.port,
-		secure: config.mailer.forcetls,
-		auth: {
-			user: config.mailer.user,
-			pass: config.mailer.pass
-		}
-	});
+  return new Promise((resolve, reject) => {
+    try {
+      let transporter = nodemailer.createTransport({
+        host: config.mailer.host,
+        port: config.mailer.port,
+        secure: config.mailer.forcetls,
+        auth: {
+          user: config.mailer.user,
+          pass: config.mailer.pass
+        }
+      });
 
-	let mailOptions = {
-		from: config.mailer.defaultFromDisplayName + ' <' + config.mailer.user +'@'+config.mailer.host+'>',
-		to: to,
-		subject: config.mailer.defaultSubject,
-		text: content,
-		html: content
-	};
+      let mailOptions = {
+        from: config.mailer.defaultFromDisplayName + ' <' + config.mailer.user +'@'+config.mailer.host+'>',
+        to: to,
+        subject: config.mailer.defaultSubject,
+        text: content,
+        html: content
+      };
 
-	transporter.sendMail(mailOptions, (error, info) => {
-		if(error) {
-			console.log(error);
-		}
-		console.log('Message %s sent: %s', info.messageId, info.response);
-	});
-
+      transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+          reject(error);
+        } else {
+          resolve(info);
+          // console.log('Message %s sent: %s', info.messageId, info.response);
+        }
+      });
+    } catch(err) {
+      reject(err);
+    }
+  });
 }
 
 function subscribeToMailList(address, list) {
