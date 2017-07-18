e09f59dce6f6872ff431991a93a59e2c120081d6
Szász Márton <szaszm@users.noreply.github.com>
2017-07-18 19:05:26 +0200

    Email sending support (#38)
    * Initial email sending support
    
    The code really needs some love that lasts more than half an hour.
    Need to get hardcoded values off email.js to config.
    Need to make sendMail async, because email sending is slow.
    
    * Finished basic email sending support
    
    * converted utils.sendMail to return a promise

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,41 @@
+"use strict";
+const nodemailer = require('nodemailer');
+const config = require('../config/config.js');
+
+
 function sendMail(to, content) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    try {
+      let transporter = nodemailer.createTransport({
+        host: config.mailer.host,
+        port: config.mailer.port,
+        secure: config.mailer.forcetls,
+        auth: {
+          user: config.mailer.user,
+          pass: config.mailer.pass
+        }
+      });
+
+      let mailOptions = {
+        from: config.mailer.defaultFromDisplayName + ' <' + config.mailer.user +'@'+config.mailer.host+'>',
+        to: to,
+        subject: config.mailer.defaultSubject,
+        text: content,
+        html: content
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+          reject(error);
+        } else {
+          resolve(info);
+          // console.log('Message %s sent: %s', info.messageId, info.response);
+        }
+      });
+    } catch(err) {
+      reject(err);
+    }
+  });
 }
 
 function subscribeToMailList(address, list) {
