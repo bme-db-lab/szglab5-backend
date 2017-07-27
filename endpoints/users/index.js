e7418eb02c94d13bff73ac0db11524f5e626234f
Mark Fejes <fejes.mark92@gmail.com>
2017-07-27 12:27:50 +0200

    add list users endpoint

--- endpoints/users/index.js
+++ endpoints/users/index.js
@@ -1,9 +1,11 @@
 const auth = require('../../middlewares/auth.js');
 const getUser = require('./get.js');
 const updateUser = require('./update.js');
+const listUsers = require('./list.js');
 
 module.exports = (app) => {
   app.use('/users/*', auth);
   app.get('/users/:id', getUser);
+  app.get('/users', listUsers);
   app.patch('/users/:id', updateUser);
 };
