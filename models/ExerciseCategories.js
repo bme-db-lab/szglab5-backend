80d7ff6d442d20e683ae4ac6b782183c5002e09c
Mark Fejes <fejes.mark92@gmail.com>
2017-07-27 13:39:08 +0200

    fix news delete response, and start working on questions

--- models/ExerciseCategories.js
+++ models/ExerciseCategories.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
 
   ExerciseCategories.associate = (models) => {
     ExerciseCategories.hasMany(models.ExerciseSheets);
+    ExerciseCategories.hasMany(models.Questions);
     ExerciseCategories.belongsTo(models.Courses);
   };
 
