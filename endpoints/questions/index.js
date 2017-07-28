ac83b68306a6e557b699a6976b620f35dd91bfab
Mark Fejes <fejes.mark92@gmail.com>
2017-07-28 10:48:47 +0200

    add generate pdf endpoint

--- endpoints/questions/index.js
+++ endpoints/questions/index.js
@@ -3,10 +3,12 @@ const deleteQuestion = require('./delete.js');
 const getQuestion = require('./get.js');
 const listQuestions = require('./list.js');
 const updateQuestions = require('./update.js');
+const generatePDF = require('./generatePDF.js');
 
 module.exports = (app) => {
   app.get('/questions', listQuestions);
   app.get('/questions/:id', getQuestion);
+  app.get('/generatePDF', generatePDF);
   app.delete('/questions/:id', deleteQuestion);
   app.patch('/questions/:id', updateQuestions);
   app.post('/questions', addQuestion);
