44b1d31c318453ca11fb8ea3a835c92592d48124
Mark Fejes <fejes.mark92@gmail.com>
2017-08-01 13:33:29 +0200

    add user response

--- endpoints/users/add.js
+++ endpoints/users/add.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const config = require('./../../config/config');
 const { genErrorObj } = require('../../utils/utils.js');
-const { checkIfExist } = require('../../utils/jsonapi.js');
+const { checkIfExist, genJSONApiResByRecord } = require('../../utils/jsonapi.js');
 const { getDB } = require('../../db/db.js');
 
 module.exports = async (req, res) => {
@@ -15,7 +15,8 @@ module.exports = async (req, res) => {
     }
     const newUser = await db.Users.create(data.attributes);
     checkIfExist(newUser);
-    res.status(201).send();
+    const response = await genJSONApiResByRecord(db, 'Users', newUser);
+    res.status(201).send(response);
   } catch (err) {
     res.status(500).send(genErrorObj(err.message));
   }
