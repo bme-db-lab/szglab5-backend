985e4d105947bf9b063d5b6ebf822e23114eeb00
Mark Fejes <fejes.mark92@gmail.com>
2017-08-01 13:38:07 +0200

    newpwd instead of password

--- endpoints/users/add.js
+++ endpoints/users/add.js
@@ -9,9 +9,10 @@ module.exports = async (req, res) => {
     const { data } = req.body;
 
     const db = getDB();
-    if (data.attributes.password) {
-      const newPasswordHash = await bcrypt.hash(data.attributes.password, config.bcrypt.saltRounds);
+    if (data.attributes.newpwd) {
+      const newPasswordHash = await bcrypt.hash(data.attributes.newpwd, config.bcrypt.saltRounds);
       data.attributes.password = newPasswordHash;
+      delete data.attributes.newpwd;
     }
     const newUser = await db.Users.create(data.attributes);
     checkIfExist(newUser);
