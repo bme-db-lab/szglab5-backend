45b13bdee470c9c6386d152ad47206bc38c1df80
Mark Fejes <fejes.mark92@gmail.com>
2017-08-01 11:47:33 +0200

    add user endpoint

---
+++ endpoints/users/add.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+const config = require('./../../config/config');
+const { genErrorObj } = require('../../utils/utils.js');
+const { checkIfExist } = require('../../utils/jsonapi.js');
+const { getDB } = require('../../db/db.js');
+
+module.exports = async (req, res) => {
+  try {
+    const { data } = req.body;
+
+    const db = getDB();
+    if (data.attributes.password) {
+      const newPasswordHash = await bcrypt.hash(data.attributes.password, config.bcrypt.saltRounds);
+      data.attributes.password = newPasswordHash;
+    }
+    const newUser = await db.Users.create(data.attributes);
+    checkIfExist(newUser);
+    res.status(201).send();
+  } catch (err) {
+    res.status(500).send(genErrorObj(err.message));
+  }
+};
--- endpoints/users/index.js
+++ endpoints/users/index.js
@@ -2,10 +2,12 @@ const auth = require('../../middlewares/auth.js');
 const getUser = require('./get.js');
 const updateUser = require('./update.js');
 const listUsers = require('./list.js');
+const addUser = require('./add.js');
 
 module.exports = (app) => {
   app.use('/users*', auth);
   app.get('/users/:id', getUser);
   app.get('/users', listUsers);
   app.patch('/users/:id', updateUser);
+  app.post('/users', addUser);
 };
