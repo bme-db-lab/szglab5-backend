1d88c1788481d56754d7c0c8afbf4e1e8be4be06
Mark Fejes <fejes.mark92@gmail.com>
2017-08-01 14:25:21 +0200

    add filter support to users list

--- endpoints/users/list.js
+++ endpoints/users/list.js
@@ -4,8 +4,40 @@ const { getDB } = require('../../db/db.js');
 
 module.exports = (req, res) => {
   try {
+    const queryObj = {};
+    if (req.query.filter && req.query.filter.search) {
+      queryObj.where = {
+        $or: [
+          {
+            loginName: {
+              $like: `%${req.query.filter.search}%`
+            }
+          },
+          {
+            displayName: {
+              $like: `%${req.query.filter.search}%`
+            }
+          },
+          {
+            neptun: {
+              $like: `%${req.query.filter.search}%`
+            }
+          }
+        ]
+      };
+    }
+
+    if (req.query.limit) {
+      queryObj.limit = parseInt(req.query.limit, 10);
+    }
+
+    if (req.query.offset) {
+      queryObj.offset = parseInt(req.query.offset, 10);
+    }
+    console.log(JSON.stringify(queryObj));
+
     const db = getDB();
-    db.Users.findAll()
+    db.Users.findAll(queryObj)
       .then(genJSONApiResByRecords.bind(null, db, 'Users'))
       .then((response) => {
         res.send(response);
