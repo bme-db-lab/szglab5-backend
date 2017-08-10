407409131b0b319c6602434dea71c73b4fa6a8b6
Mark Fejes <fejes.mark92@gmail.com>
2017-08-10 21:25:51 +0200

    add roles to parser

--- courses/VM010/VM010.js
+++ courses/VM010/VM010.js
@@ -13,10 +13,33 @@ module.exports = async (semesterId) => {
   try {
     const students = await parseStudents(semesterId);
     await seedDBwithObjects(db, 'Users', students);
+    // set user roles
+    const studentRole = await db.Roles.findOne({ where: { name: 'STUDENT' } });
+    for (const student of students) {
+      const studentRecord = await db.Users.findOne({ where: { neptun: student.data.neptun } });
+      const userRole = [{
+        data: {
+          UserId: studentRecord.dataValues.id,
+          RoleId: studentRole.dataValues.id
+        }
+      }];
+      await seedDBwithObjects(db, 'UserRoles', userRole);
+    }
     const exercises = await parseExercises(semesterId);
     await seedDBwithObjects(db, 'ExerciseTypes', exercises);
     const staff = await parseStaff();
     await seedDBwithObjects(db, 'Users', staff);
+    const demonRole = await db.Roles.findOne({ where: { name: 'DEMONSTRATOR' } });
+    for (const staffMember of staff) {
+      const staffMemberRecord = await db.Users.findOne({ where: { email_official: staffMember.data.email_official } });
+      const userRole = [{
+        data: {
+          UserId: staffMemberRecord.dataValues.id,
+          RoleId: demonRole.dataValues.id
+        }
+      }];
+      await seedDBwithObjects(db, 'UserRoles', userRole);
+    }
     const groups = await parseGroups(semesterId);
     await seedDBwithObjects(db, 'StudentGroups', groups);
     const regs = await parseStudentRegs(semesterId);
