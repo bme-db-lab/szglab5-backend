a80ec6627e831348dfaed438af4a454d66db2697
Mark Fejes <fejes.mark92@gmail.com>
2017-08-15 13:26:04 +0200

    add includes to GET /event-templates/1

--- endpoints/eventTemplates/get.js
+++ endpoints/eventTemplates/get.js
@@ -2,7 +2,7 @@ const { genErrorObj } = require('../../utils/utils.js');
 const { genJSONApiResByRecord, checkIfExist } = require('../../utils/jsonapi.js');
 const { getDB } = require('../../db/db.js');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   try {
     const reqId = req.params.id;
     const reqIdNum = parseInt(reqId, 10);
@@ -12,20 +12,37 @@ module.exports = (req, res) => {
     }
 
     const db = getDB();
-    db.EventTemplates.findById(reqIdNum)
-      .then(checkIfExist)
-      .then(genJSONApiResByRecord.bind(null, db, 'EventTemplates'))
-      .then((response) => {
-        res.send(response);
-      })
-      .catch((err) => {
-        if (err.notFound) {
-          res.status(404).send(genErrorObj(err.message));
-          return;
-        }
-        res.status(500).send(genErrorObj(err.message));
+    const eventTemplate = await db.EventTemplates.findById(reqIdNum);
+    checkIfExist(eventTemplate);
+    const response = await genJSONApiResByRecord(db, 'EventTemplates', eventTemplate);
+    response.included = [];
+    // Included: Deliverable-Templates
+    if (response.data.relationships.DeliverableTemplates.data.length !== 0) {
+      for (const delItem of response.data.relationships.DeliverableTemplates.data) {
+        const delTemplate = await db.DeliverableTemplates.findById(delItem.id);
+        response.included.push({
+          id: delTemplate.dataValues.id,
+          type: 'DeliverableTemplates',
+          attributes: delTemplate.dataValues
+        });
+      }
+    }
+    // Incldued: ExerciseCategory
+    if (response.data.relationships.ExerciseCategory.data !== null) {
+      const exCat = await db.ExerciseCategories.findById(response.data.relationships.ExerciseCategory.data.id);
+      response.included.push({
+        id: response.data.relationships.ExerciseCategory.data.id,
+        type: 'ExerciseCategories',
+        attributes: exCat.dataValues
       });
+    }
+
+    res.send(response);
   } catch (err) {
+    if (err.notFound) {
+      res.status(404).send(genErrorObj(err.message));
+      return;
+    }
     res.status(500).send(genErrorObj(err.message));
   }
 };
