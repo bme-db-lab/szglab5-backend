03a579d44d291f93c9419d7ee7148022f3e8758a
Mark Fejes <fejes.mark92@gmail.com>
2017-08-15 13:38:20 +0200

    add included to LIST event-templates

--- endpoints/eventTemplates/list.js
+++ endpoints/eventTemplates/list.js
@@ -4,17 +4,34 @@ const { genErrorObj } = require('../../utils/utils.js');
 const { genJSONApiResByRecords } = require('../../utils/jsonapi.js');
 const { getDB } = require('../../db/db.js');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   try {
     const db = getDB();
-    db.EventTemplates.findAll()
-      .then(genJSONApiResByRecords.bind(null, db, 'EventTemplates'))
-      .then((response) => {
-        res.send(response);
-      })
-      .catch((err) => {
-        res.status(500).send(genErrorObj(err.message));
-      });
+    const eventTemplates = await db.EventTemplates.findAll();
+    const response = await genJSONApiResByRecords(db, 'EventTemplates', eventTemplates);
+    response.included = [];
+    for (const eventTemplate of response.data) {
+      if (eventTemplate.relationships.DeliverableTemplates.data.length !== 0) {
+        for (const delItem of eventTemplate.relationships.DeliverableTemplates.data) {
+          const delTemplate = await db.DeliverableTemplates.findById(delItem.id);
+          response.included.push({
+            id: delTemplate.dataValues.id,
+            type: 'DeliverableTemplates',
+            attributes: delTemplate.dataValues
+          });
+        }
+      }
+      // Incldued: ExerciseCategory
+      if (eventTemplate.relationships.ExerciseCategory.data !== null) {
+        const exCat = await db.ExerciseCategories.findById(eventTemplate.relationships.ExerciseCategory.data.id);
+        response.included.push({
+          id: eventTemplate.relationships.ExerciseCategory.data.id,
+          type: 'ExerciseCategories',
+          attributes: exCat.dataValues
+        });
+      }
+    }
+    res.send(response);
   } catch (err) {
     res.status(500).send(genErrorObj(err.message));
   }
