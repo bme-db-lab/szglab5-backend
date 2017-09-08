47ba5cf451f7eddb224be14974079b4849f71b6f
Szász Márton <szaszm01@gmail.com>
2017-09-08 23:04:07 +0200

    began refactoring for multiple handouts per zip

--- utils/generateSheet.js
+++ utils/generateSheet.js
@@ -1,14 +1,15 @@
 const xmlbuilder = require('xmlbuilder');
 
 
-function magicPasswordGeneratorThatDefinitelyShouldNotBeHereOrImplementedLikeThis(student) {
-  const md5 = require('crypto').createHash('md5');
-  const year = (new Date()).getFullYear();
-  md5.update(year.toString() + student.id);
-  return md5.digest('base64').substr(0, 10);
-}
+function generateHandout(event) {
+  const exerciseSheet = event.ExerciseSheet;
+  console.log(exerciseSheet);
+  const exerciseCategory = exerciseSheet.ExerciseCategory;
+  const exerciseType = exerciseSheet.ExerciseType;
+  const student = event.StudentRegistration.User;
+  const demonstrator = event.Demonstrator;
 
-module.exports = (exerciseCategory, exerciseType, student, demonstrator, event) => {
+
   const categoryId = exerciseCategory.id;
   const categoryName = exerciseCategory.type;
   const exerciseTypeId = exerciseType.id;
@@ -17,42 +18,49 @@ module.exports = (exerciseCategory, exerciseType, student, demonstrator, event) 
   const studentName = student.displayName;
   const demonstratorName = demonstrator.displayName;
   const timeOfEvent = event.date;
-  const initialPassword = magicPasswordGeneratorThatDefinitelyShouldNotBeHereOrImplementedLikeThis(student);
+  const initialPassword = student.initPassword;
 
-  const xmlNode = xmlbuilder.create({
-    handouts: {
-      handout: {
-        'exercise-description': {
-          '@category': categoryId,
-          '@category-name': categoryName,
-          '@type': exerciseTypeId,
-          '@shortname': shortName,
-          '#text': exerciseTypeName
+  return {
+    'exercise-description': {
+      '@category': categoryId,
+      '@category-name': categoryName,
+      '@type': exerciseTypeId,
+      '@shortname': shortName,
+      '#text': exerciseTypeName
+    },
+    entries: {
+      entry: [
+        {
+          '@description': 'Hallgató neve',
+          '#text': studentName
         },
-        entries: {
-          entry: [
-            {
-              '@description': 'Hallgató neve',
-              '#text': studentName
-            },
-            {
-              '@description': 'Mérésvezető',
-              '#text': demonstratorName
-            },
-            {
-              '@description': 'Mérés időpontja',
-              '#text': timeOfEvent.toDateString()
-            },
-            {
-              '@description': 'Portál jelszó',
-              '#text': initialPassword
-            }
-          ]
+        {
+          '@description': 'Mérésvezető',
+          '#text': demonstratorName
+        },
+        {
+          '@description': 'Mérés időpontja',
+          '#text': timeOfEvent.toDateString()
+        },
+        {
+          '@description': 'Portál jelszó',
+          '#text': initialPassword
         }
-      }
+      ]
     }
-  });
+  };
+}
 
-  const str = xmlNode.end({ pretty: true });
-  return str;
-};
+function concatHandouts(handouts) {
+  return {
+    handouts
+  };
+}
+
+function generateXml(handoutsObj) {
+  const node = xmlbuilder.create(handoutsObj);
+  return node.end({ pretty: true });
+}
+
+module.exports = event => generateXml(concatHandouts([generateHandout(event)]));
+
