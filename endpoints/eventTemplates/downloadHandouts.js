ab827f7e9d31ee87d508c3fa94901d61fd8420e0
Szász Márton <szaszm01@gmail.com>
2017-09-14 10:07:46 +0200

    correctly handle events moved to another demonstrator

--- endpoints/eventTemplates/downloadHandouts.js
+++ endpoints/eventTemplates/downloadHandouts.js
@@ -4,6 +4,7 @@ const { getDB } = require('../../db/db.js');
 const sheet = require('../../utils/generateSheet.js');
 const logger = require('../../utils/logger.js');
 
+/*
 function getEventsByDemonstrator(demonstrator, db) {
   return new Promise((resolve, reject) => {
     const events = [];
@@ -47,6 +48,47 @@ function getEventsByDemonstrator(demonstrator, db) {
       resolve(events);
     }).catch((err) => { reject(err); });
   });
+}*/
+
+function getEventsByDemonstrator(demonstrator, db) {
+  return new Promise((resolve, reject) => {
+    const events = [];
+    const promises = [];
+    demonstrator.Events.forEach((event) => {
+      promises.push(db.Events.findById(event.id, {
+        include: [
+          {
+            model: db.ExerciseSheets,
+            include: [
+              {
+                model: db.ExerciseCategories
+              },
+              {
+                model: db.ExerciseTypes
+              }
+            ]
+          },
+          {
+            model: db.StudentRegistrations,
+            include: {
+              model: db.Users
+            }
+          },
+          {
+            model: db.Users,
+            as: 'Demonstrator'
+          }
+        ]
+      }).then((result) => {
+        events.push(result);
+      }).catch((err) => {
+        throw err;
+      }));
+    });
+    Promise.all(promises).then(() => {
+      resolve(events);
+    }).catch((err) => { reject(err); });
+  });
 }
 
 
@@ -66,14 +108,8 @@ module.exports = async (req, res) => {
     /* Get events and related data by demonstrator (User) */
     const user = await db.Users.findById(userInfo.userId, {
       include: {
-        model: db.StudentGroups,
-        include: {
-          model: db.StudentRegistrations,
-          include: {
-            model: db.Events,
-            where: { EventTemplateId: req.params.id }
-          }
-        }
+        model: db.Events,
+        where: { EventTemplateId: req.params.id }
       }
     });
 
