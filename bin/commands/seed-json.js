32ec876899b37a767565f2977ba79e30d007d7b7
Mark Fejes <fejes.mark92@gmail.com>
2017-09-15 08:46:31 +0200

    add CLI confirmation for modifying commands, to avoid accidental running

--- bin/commands/seed-json.js
+++ bin/commands/seed-json.js
@@ -1,8 +1,20 @@
 const { seedDBwithJSON } = require('./../../db/seed');
 const { initDB, closeDB } = require('./../../db/db');
 const path = require('path');
+const inquirer = require('inquirer');
 
 module.exports = async (_filePath) => {
+  const confirmPromptResult = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'res',
+      message: 'Are you sure?'
+    }
+  ]);
+  if (!confirmPromptResult.res) {
+    throw new Error('Confirmation error!');
+  }
+
   const filePath = path.isAbsolute(_filePath) ? _filePath : path.join(__dirname, '../..', _filePath);
   const db = await initDB({
     force: true
