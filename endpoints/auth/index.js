494f91ab95db65f37a39dc5cf26e2a7cc51d1295
Mark Fejes <fejes.mark92@gmail.com>
2017-09-19 17:27:03 +0200

    fix admin/impersonate #37

--- endpoints/auth/index.js
+++ endpoints/auth/index.js
@@ -1,6 +1,7 @@
 const login = require('./login.js');
 const impersonate = require('./impersonate');
 const verifyToken = require('./verify-token');
+const auth = require('../../middlewares/auth.js');
 
 const epLogger = require('../../middlewares/ep-logger');
 
@@ -9,6 +10,6 @@ module.exports = (app) => {
   app.use('/auth*', epLogger);
 
   app.post('/auth/login', login);
-  app.post('/auth/impersonate', impersonate);
+  app.post('/auth/impersonate', auth, impersonate);
   app.post('/auth/verify-token', verifyToken);
 };
