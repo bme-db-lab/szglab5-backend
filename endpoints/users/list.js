5c60ea563ea84e0aabcb36b87fdf5d2434e59276
Mark Fejes <fejes.mark92@gmail.com>
2017-12-11 09:19:41 +0100

    endpoints/users/list: remove include all

--- endpoints/users/list.js
+++ endpoints/users/list.js
@@ -6,6 +6,14 @@ module.exports = async (req, res) => {
   try {
     const db = getDB();
 
+    const { roles } = req.userInfo;
+    // only ADMIN DEMONSTRATOR CORRECTOR
+    if (!roles.includes('ADMIN') && !roles.includes('DEMONSTRATOR') && !roles.includes('CORRECTOR')) {
+      res.status(403).send(genErrorObj('Unathorized'));
+      return;
+    }
+
+
     const queryObj = {};
     if (req.query.filter && req.query.filter.search) {
       queryObj.where = {
@@ -36,10 +44,11 @@ module.exports = async (req, res) => {
     if (req.query.offset) {
       queryObj.offset = parseInt(req.query.offset, 10);
     }
-    queryObj.include = [{ all: true }];
+    // queryObj.include = [{ all: true }];
     queryObj.attributes = ['id', 'loginName', 'displayName', 'email', 'subscribedToMailList', 'subscribedToEmailNotify', 'neptun', 'email_official'];
 
     const records = await db.Users.findAll(queryObj);
+
     const response = getJSONApiResponseFromRecords(db, 'Users', records, {
       includeModels: []
     });
