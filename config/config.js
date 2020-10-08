849aac910d348693a791d26f4ebacdca1d0f0935
Mark Fejes <mark.fejes@supercharge.io>
2020-10-08 21:54:16 +0200

    feat(get-handout): change default handout config

--- config/config.js
+++ config/config.js
@@ -77,7 +77,7 @@ const defaultConfig = {
     ]
   },
   generatedFilesPath: path.join(__dirname, '../generated'),
-  handoutGeneratorRoot: '/home/handout/adatlabor-feladatlap'
+  handoutGeneratorRoot: '/home/backend/adatlabor-feladatlap'
 };
 
 let specConfig = {};
