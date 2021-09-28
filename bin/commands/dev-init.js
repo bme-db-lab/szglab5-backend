a61c03302408ef8d1332e686594062418152352b
Mark Fejes <mark.fejes@supercharge.io>
2021-09-28 19:50:04 +0200

    chore: change academicyear to current

--- bin/commands/dev-init.js
+++ bin/commands/dev-init.js
@@ -46,7 +46,7 @@ module.exports = async (allUser, genPass, hallgatok, beosztas, basePath, initAdm
     }
     // Initialize Semester
     const semesterData = [{ data: {
-      academicyear: '2020/2021',
+      academicyear: '2021/2022',
       academicterm: 1,
       CourseId: 1
     } }];
@@ -54,7 +54,7 @@ module.exports = async (allUser, genPass, hallgatok, beosztas, basePath, initAdm
     const qResult = await db.Semesters.findOne({
       attributes: ['id'],
       where: {
-        academicyear: '2020/2021',
+        academicyear: '2021/2022',
         academicterm: 1,
         CourseId: 1
       }
