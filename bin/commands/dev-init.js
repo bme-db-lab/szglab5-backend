1793bec0ac77ac7bf433561a583da0c2f10a2fef
Mark Fejes <fejes.mark92@gmail.com>
2023-09-16 19:58:40 +0200

    chore: prepare for 2023/2024 semester

--- bin/commands/dev-init.js
+++ bin/commands/dev-init.js
@@ -46,7 +46,7 @@ module.exports = async (allUser, genPass, hallgatok, beosztas, basePath, initAdm
     }
     // Initialize Semester
     const semesterData = [{ data: {
-      academicyear: '2022/2023',
+      academicyear: '2023/2024',
       academicterm: 1,
       CourseId: 1
     } }];
@@ -54,7 +54,7 @@ module.exports = async (allUser, genPass, hallgatok, beosztas, basePath, initAdm
     const qResult = await db.Semesters.findOne({
       attributes: ['id'],
       where: {
-        academicyear: '2022/2023',
+        academicyear: '2023/2024',
         academicterm: 1,
         CourseId: 1
       }
